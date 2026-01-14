330fc015d59998ca5059ff0cf2f617b9e09bd939
aasha-g <aashagowda1095@gmail.com>
2026-01-14 22:47:37 +0800

    Update 4_ForLoopSumofNaturalNumbers.java

--- 2_Loops/4_ForLoopSumofNaturalNumbers.java
+++ 2_Loops/4_ForLoopSumofNaturalNumbers.java
@@ -1,1 +1,40 @@
+package internshipAssignments_1;
+
+public class SumOfNaturalNumbers {
+
+	public static void main(String[] args) {
+		
+    int N = 10, sum=0, i=1;
+		
+	
+    for(i=1;i<=N;i++)
+    {
+      sum = sum +i;
+    }
+    
+    System.out.println("sum of natural numbers is:"+""+sum);
+    
+    
+    /*  while (i<=N) 
+	{
+		sum = sum + i;
+		i++;
+	}
+	
+	
+	System.out.println( "Sum of natural numbers is:"   + " " + sum);*/
+	
+    
+    
+    
+    
+    
+    
+    
+    
+  
+	         
+	}
+
+}
 
