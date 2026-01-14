7573f5b2a39d32b6ced6482011aa6be044676ceb
aasha-g <aashagowda1095@gmail.com>
2026-01-14 22:32:48 +0800

    Update 1_ForLoop.java

--- 2_Loops/1_ForLoop.java
+++ 2_Loops/1_ForLoop.java
@@ -1,1 +1,28 @@
+package internshipAssignments_1;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter a number : ");
+		int n = sc.nextInt();
+		
+		long factorial = 1;
+		
+		
+		for(int i=1;i<=n;i++)
+		{
+			factorial *= i;
+		}
+		
+		System.out.println("Factorial of "+n+ " is: "+factorial);
+		sc.close();
+
+	}
+        
+}
 
