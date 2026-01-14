f4522a7d72dbca4a55af877d33c5c580ea17b4a9
aasha-g <aashagowda1095@gmail.com>
2026-01-14 22:42:34 +0800

    Update 3_ForLoopMultiplication.java

--- 2_Loops/3_ForLoopMultiplication.java
+++ 2_Loops/3_ForLoopMultiplication.java
@@ -1,1 +1,31 @@
+package internshipAssignments_1;
+
+import java.util.Scanner;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		 
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+       
+        for(int i =1;i<=10;i++) 
+        {
+         System.out.println(number+"x"+ i +""+"="+""+(number*i));	
+        }
+        sc.close();
+		
+     /*   int i = 1;
+        while(i<=10)
+        {
+        	 System.out.println(number+"x"+ i +""+"="+""+(number*i));
+        	 i++;
+        } */	 
+        
+	}
+
+}
 
