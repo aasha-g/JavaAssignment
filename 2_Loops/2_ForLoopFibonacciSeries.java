cd11e956e7b71593f04a9b2b5ffc50f4e13da8e0
aasha-g <aashagowda1095@gmail.com>
2026-01-14 22:38:07 +0800

    Update 2_ForLoopFibonacciSeries.java

--- 2_Loops/2_ForLoopFibonacciSeries.java
+++ 2_Loops/2_ForLoopFibonacciSeries.java
@@ -1,1 +1,29 @@
+package internshipAssignments_1;
+
+import java.util.Scanner;
+
+public class Fibonacciseries {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter number of terms: ");
+        int n = sc.nextInt();
+
+        int a = 0, b = 1;
+
+        System.out.print("Fibonacci Series: ");
+
+        for (int i = 4; i <= n; i++) {  
+            System.out.print(a + " ");  //a=0,1,1,2
+
+            int next = a + b; //1,2,3,5
+            a = b; //a=1 ,1,2,3 
+            b = next;//b=1,2,3,5
+        }
+
+        sc.close();   
+			  
+		   }
+	}
+
 
