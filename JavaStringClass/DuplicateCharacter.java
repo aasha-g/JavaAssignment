efa4b287d4cac69f74771b9021b6d6e95ee533ff
aasha-g <aashagowda1095@gmail.com>
2026-01-16 19:32:48 +0800

    Update DuplicateCharacter.java

--- JavaStringClass/DuplicateCharacter.java
+++ JavaStringClass/DuplicateCharacter.java
@@ -1,1 +1,41 @@
+package stringAssignment;
+
+import java.util.Scanner;
+
+public class DuplicateCharacters {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine().toLowerCase();     
+
+        char[] chars = str.toCharArray();             
+
+        System.out.print("Duplicate characters are:");
+
+            for (int i = 0; i < chars.length; i++) {        
+
+            if (chars[i] == ' ')                           
+                continue;                     
+
+            int count = 1;
+
+            for (int j = i + 1; j < chars.length; j++) {  
+                if (chars[i] == chars[j]) {               
+                    count++;
+                  
+                }
+            }
+
+            if (count > 1 && chars[i] != '0') {          
+                System.out.print(chars[i]);
+            }
+        }
+
+       sc.close();
+
+	}
+
+}
 
