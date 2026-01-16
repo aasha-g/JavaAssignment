bc95a72e7f248caa029a6f400216e9957905ad60
aasha-g <aashagowda1095@gmail.com>
2026-01-16 19:31:35 +0800

    Update CompareString.java

--- JavaStringClass/CompareString.java
+++ JavaStringClass/CompareString.java
@@ -1,1 +1,19 @@
+package stringAssignment;
 
+public class CompareString {
+
+	public static void main(String[] args) {
+		
+		        String str1 = "Hello";
+		        String str2 = "Hello";
+
+		        if (str1.equals(str2)) {
+		            System.out.println("Strings are equal");
+		        } else {
+		            System.out.println("Strings are not equal");
+		        }
+		    
+		
+	}
+
+}
