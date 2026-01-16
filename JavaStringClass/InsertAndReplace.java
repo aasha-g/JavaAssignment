da2e44a4da7a2b0b8fd0fe10ea65b47a3892b70b
aasha-g <aashagowda1095@gmail.com>
2026-01-16 19:33:16 +0800

    Update InsertAndReplace.java

--- JavaStringClass/InsertAndReplace.java
+++ JavaStringClass/InsertAndReplace.java
@@ -1,1 +1,22 @@
+package stringAssignment;
+
+public class InsertAndReplace {
+
+	public static void main(String[] args) {
+		String original = "I love Java";
+        System.out.println("Original: " + original);  //I love Java
+
+        
+        String replaced = original.replace("Java", "Coding");  //Java--->Coding
+        System.out.println("After Replace: " + replaced);     //I love Coding
+
+        
+        String target = "love";
+        int index = original.indexOf(target); // index of love-2
+        
+        String inserted = original.substring(0, index) + "really " + original.substring(index);
+        System.out.println("After Insert: " + inserted);
+	}
+          //substring(0,2)-->I +really+ ,substring(2)-->love java
+}
 
