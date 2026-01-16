ad730e6ab45f98edbae3e1313782497b20c233df
aasha-g <aashagowda1095@gmail.com>
2026-01-16 19:34:20 +0800

    Update ReverseString.java

--- JavaStringClass/ReverseString.java
+++ JavaStringClass/ReverseString.java
@@ -1,1 +1,27 @@
+package stringAssignment;
+
+import java.util.Scanner;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");   //Java
+        String input = sc.nextLine();
+
+        String reversed = "";
+
+                                                                  //length=4
+        for (int i = input.length() - 1; i >= 0; i--) {           //i=3
+            reversed = reversed + input.charAt(i);        //points at 3-->a
+        }
+
+        System.out.println("Reversed string: " + reversed);
+
+        sc.close();
+	}
+
+}
 
