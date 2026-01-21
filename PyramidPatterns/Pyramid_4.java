0bd11917274bbe7631e2ce1a838805628dea88ce
aasha-g <aashagowda1095@gmail.com>
2026-01-21 20:56:52 +0800

    Update Pyramid_4.java

--- PyramidPatterns/Pyramid_4.java
+++ PyramidPatterns/Pyramid_4.java
@@ -1,1 +1,24 @@
+package internshipAssignments_1;
 
+public class Pyramid_4 {
+
+	public static void main(String[] args) {
+		
+		int n = 5; 
+
+        for (int r = n; r >= 1; r--) {
+            
+              for (int s = 1; s <= (n - r); s++) 
+              {
+                System.out.print("  "); 
+            }
+            
+              for (int x = 1; x <= r; x++)
+              {
+                System.out.print("1 ");
+            }
+            
+            System.out.println();
+        }
+    }
+}
