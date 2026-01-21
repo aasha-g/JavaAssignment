03989b570b031c5ad538dc958e5fd19f05f76c07
aasha-g <aashagowda1095@gmail.com>
2026-01-21 20:57:17 +0800

    Update Pyramid_5.java

--- PyramidPatterns/Pyramid_5.java
+++ PyramidPatterns/Pyramid_5.java
@@ -1,1 +1,21 @@
+package internshipAssignments_1;
+
+public class Pyramid_5 {
+
+	public static void main(String[] args) {
+        int r,x;
+		
+		for(r=1;r<=5;r++)                 //r=1
+		{
+		   for(x=1;x<=r;x++)              //x=1
+		{
+			System.out.print(r+" ");      //1 
+			                             //2 2
+		}	
+			System.out.println();
+		}
+
+	}
+
+}
 
