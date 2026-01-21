a6fb979e69f50eb150664975c0acc34e10df0cc2
aasha-g <aashagowda1095@gmail.com>
2026-01-21 20:55:28 +0800

    Update Pyramid_1.java

--- PyramidPatterns/Pyramid_1.java
+++ PyramidPatterns/Pyramid_1.java
@@ -1,1 +1,21 @@
+package internshipAssignments_1;
+
+public class Pyramid_1 {
+
+	public static void main(String[] args) {
+		
+		int r,n;
+		
+		for(r=1;r<=5;r++)                 //r=1,2
+		{
+		   for(n=1;n<=r;n++)              //n=1,(1,2)
+		{
+			System.out.print("1 ");      //1 
+			                             //1 1
+		}	
+			System.out.println();
+		}
+	}
+
+}
 
