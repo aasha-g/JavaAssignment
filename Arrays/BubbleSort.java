f9dc38649934ede5a04e8457fab6507efeaf6d7c
aasha-g <aashagowda1095@gmail.com>
2026-01-28 11:20:38 +0800

    Update BubbleSort.java

--- Arrays/BubbleSort.java
+++ Arrays/BubbleSort.java
@@ -1,1 +1,31 @@
+package arrays;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		 int[] arr = {5, 2, 9, 1, 3};
+
+	        
+	        for (int i = 0; i < arr.length - 1; i++) {
+	            for (int j = 0; j < arr.length - 1 - i; j++) {
+	                if (arr[j] > arr[j + 1]) {
+	                   
+	                    int temp = arr[j];
+	                    arr[j] = arr[j + 1];
+	                    arr[j + 1] = temp;
+	                }
+	            }
+	        }
+
+	        
+	        System.out.println("Sorted Array:");
+	        for (int num : arr) {
+	            System.out.print(num + " ");
+	        }
+	    }
+	}
+
+	
+
+
 
