9c5edd5397aac43fa9bf18aaee9cf5d1a4e1252d
aasha-g <aashagowda1095@gmail.com>
2026-02-03 13:45:03 +0800

    Update Overriding.java

--- Oops.java/MethodOverriding/Overriding.java
+++ Oops.java/MethodOverriding/Overriding.java
@@ -1,1 +1,33 @@
+package methodOverriding;
+
+class Employee
+{
+	double salary()
+	{
+		return 3000;
+	}
+}
+
+class Manager extends Employee
+{
+	double salary()
+	{
+		return 5000;
+	}
+}
+
+
+public class Overriding {
+
+	public static void main(String[] args) {
+		
+    Manager m1 = new Manager();
+    Employee m2 = new Manager();
+    Employee m3 = new Employee();
+    System.out.println(m1.salary());
+    System.out.println(m2.salary());
+    System.out.println(m3.salary());
+    }
+
+}
 
