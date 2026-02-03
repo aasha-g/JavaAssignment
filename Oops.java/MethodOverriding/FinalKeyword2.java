94d35d893e00ec856a1926a24fe8486ff5514925
aasha-g <aashagowda1095@gmail.com>
2026-02-03 13:52:07 +0800

    Update FinalKeyword2.java

--- Oops.java/MethodOverriding/FinalKeyword2.java
+++ Oops.java/MethodOverriding/FinalKeyword2.java
@@ -1,1 +1,23 @@
+package methodOverriding;
+class Test1
+{
+	final void m()
+	{
+		System.out.println("m method from Test1");
+	}
+	
+class Test2 extends Test1
+{
+	//void m()               //can't do overriding for a method m
+	{
+		System.out.println("m method from Test2");
+	}
+}
+public class FinalKeyword2 {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}}
