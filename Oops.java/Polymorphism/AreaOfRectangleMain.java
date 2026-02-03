bec8035081114dd61df2fa98cfbb1c8a9f73a306
aasha-g <aashagowda1095@gmail.com>
2026-02-03 13:42:08 +0800

    Update AreaOfRectangleMain.java

--- Oops.java/Polymorphism/AreaOfRectangleMain.java
+++ Oops.java/Polymorphism/AreaOfRectangleMain.java
@@ -1,1 +1,13 @@
+package oopsPolymorphism;
 
+public class RectangleMain {
+
+	public static void main(String[] args) {
+		
+		Rectangle t=new Rectangle();
+		System.out.println(t.Area());
+		
+
+	}
+
+}
