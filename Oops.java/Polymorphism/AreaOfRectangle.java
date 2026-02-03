6dad4b189f9efa7d548dbbc6af3dd5058f6d73bd
aasha-g <aashagowda1095@gmail.com>
2026-02-03 13:41:18 +0800

    Update AreaOfRectangle.java

--- Oops.java/Polymorphism/AreaOfRectangle.java
+++ Oops.java/Polymorphism/AreaOfRectangle.java
@@ -1,1 +1,24 @@
 
+package oopsPolymorphism;
+
+public class Rectangle {
+    
+	int length,width;
+	
+	Rectangle()
+	{
+		length=4;
+		width=5;
+	}
+	
+	Rectangle(int l,int w)
+	{
+		length=l;
+		width=w;
+	}
+	
+	int Area()
+	{
+		return(length*width);
+	}
+}
