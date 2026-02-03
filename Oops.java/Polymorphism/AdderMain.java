1fb840fa5dee6041794a3f9f61da9fd4eccf3f0d
aasha-g <aashagowda1095@gmail.com>
2026-02-03 13:40:24 +0800

    Update AdderMain.java

--- Oops.java/Polymorphism/AdderMain.java
+++ Oops.java/Polymorphism/AdderMain.java
@@ -1,1 +1,15 @@
+package oopsPolymorphism;
+
+public class AdderMain {
+
+	public static void main(String[] args) {
+		
+		Adder A1 = new Adder();
+		A1.sum();
+		A1.sum(30.3, 90);
+		A1.sum(3,4);
+
+	}
+
+}
 
