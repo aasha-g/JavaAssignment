37c12d49eda3320c133c16af1e1dd86c77ba0a21
aasha-g <aashagowda1095@gmail.com>
2026-02-03 13:53:26 +0800

    Update FinalKeyword3.java

--- Oops.java/MethodOverriding/FinalKeyword3.java
+++ Oops.java/MethodOverriding/FinalKeyword3.java
@@ -1,1 +1,24 @@
+package methodOverriding;
 
+final class Test1
+{
+	void m()
+	{
+		System.out.println("m method from Test1");
+	}
+	
+class Test2 extends Test1 // can not extend to class Test1
+{
+	void m()               
+	{
+		System.out.println("m method from Test2");
+	}
+}
+
+public class FinalKeyword3 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
