41a31ee1f379b0d31b87c7970f5acf75ca4de338
aasha-g <aashagowda1095@gmail.com>
2026-02-03 13:35:52 +0800

    Update MultilevelInheritance.java

--- Oops.java/InheritanceTypes/MultilevelInheritance.java
+++ Oops.java/InheritanceTypes/MultilevelInheritance.java
@@ -1,1 +1,43 @@
+package inheritanceTypes;
+
+class X
+{
+	int x=10;
+	void display()
+	{
+	  System.out.println(x);
+	}
+	
+}
+
+class Y extends X
+{
+	int y=20;
+	void show()
+	{
+		System.out.println(y);
+	}
+}
+
+class Z extends Y
+{
+	int z=30;
+	void print()
+	{
+		System.out.println(z);
+	}
+}
+
+public class MultilevelInheritance {
+
+	public static void main(String[] args) {
+		
+		Z zobj = new Z();
+	      	      
+		zobj.display();
+		zobj.show();
+		zobj.print();
+	}
+
+}
 
