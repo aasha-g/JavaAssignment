b0f525175a7eaaa8ecb4546203b595d2061a8aaa
aasha-g <aashagowda1095@gmail.com>
2026-02-03 13:39:44 +0800

    Update Adder.java

--- Oops.java/Polymorphism/Adder.java
+++ Oops.java/Polymorphism/Adder.java
@@ -1,1 +1,37 @@
+package oopsPolymorphism;
 
+public class Adder {           //method overloading
+
+	
+		int a =10,b=20;
+		
+		void sum()                    //method name should be same
+		{
+			System.out.println(a+b);
+		}
+		
+		
+	/*	int sum()                    //Duplicate:don't consider the return type
+		{
+			return(a+b);
+		}*/
+		
+		
+		
+		
+		
+		void sum(int x,int y)         //No. of parameters should be different
+	    {
+		    System.out.println(x+y);
+	    }
+		
+		void sum(int x,double y)     //data type of parameters should be different
+		{
+			System.out.println(x+y);
+		}
+        
+		void sum(double x,int y)    //order should be different
+		{
+			System.out.println(x+y);
+		}
+}
