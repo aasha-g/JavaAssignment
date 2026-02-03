9088a67fb7904c8555602e173f1d04fcd01351b2
aasha-g <aashagowda1095@gmail.com>
2026-02-03 13:50:13 +0800

    Update FinalKeyword1.java

--- Oops.java/MethodOverriding/FinalKeyword1.java
+++ Oops.java/MethodOverriding/FinalKeyword1.java
@@ -1,1 +1,19 @@
+package methodOverriding;
+
+class A
+{
+	final int a =100;
+}
+
+public class FinalKeyword1 {
+
+	public static void main(String[] args) {
+		
+		A a1 = new A();
+		//a1.a=200;  --incorrect can not change the variable
+		System.out.println(a1.a);
+		
+	}
+
+}
 
