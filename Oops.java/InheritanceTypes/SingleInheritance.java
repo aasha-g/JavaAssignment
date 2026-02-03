91d137ee0805c13c718f15ef97888314eeb5c873
aasha-g <aashagowda1095@gmail.com>
2026-02-03 13:34:40 +0800

    Update SingleInheritance.java

--- Oops.java/InheritanceTypes/SingleInheritance.java
+++ Oops.java/InheritanceTypes/SingleInheritance.java
@@ -1,1 +1,35 @@
+package inheritanceTypes;
+
+class A
+{
+	int a=10;
+	void display()
+	{
+	  System.out.println(a);
+	}
+	
+}
+
+class B extends A
+{
+	int b=20;
+	void show()
+	{
+		System.out.println(b);
+	}
+}
+
+public class SingleInheritance {
+
+	public static void main(String[] args) {
+		
+      B bobj = new B();
+      System.out.println(bobj.a);
+      System.out.println(bobj.b);
+      
+      bobj.display();
+      bobj.show();
+	}
+
+}
 
