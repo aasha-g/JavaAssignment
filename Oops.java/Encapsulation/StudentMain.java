160e3b020a13f5153e998cedf668799029e71ae6
aasha-g <aashagowda1095@gmail.com>
2026-02-04 14:23:08 +0800

    Update StudentMain.java

--- Oops.java/Encapsulation/StudentMain.java
+++ Oops.java/Encapsulation/StudentMain.java
@@ -1,1 +1,17 @@
+package encapsulation;
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		
+		Student s = new Student();
+           
+        s.setName("asha");
+              
+        System.out.println(s.getName());
+
+       
+}
+
+}
 
