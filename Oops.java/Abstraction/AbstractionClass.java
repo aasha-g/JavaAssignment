7030f8fc1f9f4cfe149dcef5e219d35b823bdc0d
aasha-g <aashagowda1095@gmail.com>
2026-02-04 14:15:54 +0800

    Update AbstractionClass.java

--- Oops.java/Abstraction/AbstractionClass.java
+++ Oops.java/Abstraction/AbstractionClass.java
@@ -1,1 +1,37 @@
+package abstraction;
+abstract class shape1
+{
+	static int length=10; //final and static
+	static int width= 20;
+	
+	abstract void circle();     //abstract method
+	
+	static void rectangle()
+	{
+		System.out.println("This is rectangle");
+		System.out.println(length);
+		System.out.println(width);
+	}
+}
+
+public class AbstractionDemo extends shape1 {
+	
+	public void circle()   // implement the unimplemented method
+	{
+		System.out.println("This is circle");
+	}
+
+	
+
+
+	public static void main(String[] args) {
+		
+		AbstractionDemo d = new AbstractionDemo(); 
+	
+		d.circle();
+		rectangle();
+		
+		
+	}
+}
 
