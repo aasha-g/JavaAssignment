556385f8f3bab8f1b63cee19768e2b42c8b48fb5
aasha-g <aashagowda1095@gmail.com>
2026-02-04 14:13:45 +0800

    Update Poly_bank.java

--- Oops.java/Polymorphism_2/Poly_bank.java
+++ Oops.java/Polymorphism_2/Poly_bank.java
@@ -1,1 +1,22 @@
+package polymorphism_2;
 
+public class Poly_bank {
+
+	public static void main(String[] args) {
+
+		Bank b; // object of parent class
+		
+		b = new Bob();
+		//bob b1 = new bob();
+		System.out.println(" Bob : ROI " + b.getRateOfInterest());
+		
+		  b = new Citi(); 
+		  System.out.println(" CITI : ROI " + b.getRateOfInterest());
+		  
+		   b  = new Canara(); 
+		  System.out.println(" Canara : ROI " + b.getRateOfInterest());
+
+		 
+	}
+
+}
