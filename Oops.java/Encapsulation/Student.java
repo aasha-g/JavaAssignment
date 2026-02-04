3bbd201e6fdd48396f092216d78f7fa31db0d4bf
aasha-g <aashagowda1095@gmail.com>
2026-02-04 14:22:17 +0800

    Update Student.java

--- Oops.java/Encapsulation/Student.java
+++ Oops.java/Encapsulation/Student.java
@@ -1,1 +1,16 @@
 
+package encapsulation;
+
+	public class Student {
+	   
+	    private String name; // Private variables (cannot be accessed directly from outside)
+	    	    
+	    public String getName() {     // returns the value of the variable name
+	        return name;
+	    }
+	   
+	    public void setName(String name) {     //  takes a name and assigns it to the name variable
+	        this.name = name;
+	    }
+
+}
