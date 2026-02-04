0abcf990f673643615abb4e55d0b4ed087704eb8
aasha-g <aashagowda1095@gmail.com>
2026-02-04 14:16:50 +0800

    Update Interface.java

--- Oops.java/Abstraction/Interface.java
+++ Oops.java/Abstraction/Interface.java
@@ -1,1 +1,39 @@
+package abstraction;
+
+  interface shape
+{
+	int length=10; //final and static
+	int width= 20;
+	
+	void circle();     //abstract method
+	
+	default void square()
+	{
+		System.out.println("This is square");
+	}
+	
+
+	static void rectangle()
+	{
+		System.out.println("This is rectangle");
+	}
+	
+}
+
+public class InterfaceDemo  implements shape{
+	
+	public void circle()   // implement the unimplemented method
+	{
+		System.out.println("This is circle");
+	}
+
+	public static void main(String[] args) {
+		
+        shape sh=new InterfaceDemo();//Can't create object for the interface--create an object for a class
+        sh.circle();                 //and store in a variable of a interface
+        sh.square();
+        shape.rectangle();
+	}
+
+}
 
