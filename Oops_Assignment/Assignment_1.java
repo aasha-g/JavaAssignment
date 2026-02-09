e2da8beae203cab8393f9a7200e0197fff434282
aasha-g <aashagowda1095@gmail.com>
2026-02-09 11:28:59 +0800

    Update Assignment_1.java

--- Oops_Assignment/Assignment_1.java
+++ Oops_Assignment/Assignment_1.java
@@ -1,1 +1,23 @@
+package oopsAssignment;
+
+class Student {
+    String name;
+    String student_id;
+    double marks_cc;
+    double marks_ml;
+    double avg_marks;
+
+    // Parameterized Constructor
+    public Student(String name, String student_id, double marks_cc, double marks_ml) {
+        this.name = name;
+        this.student_id = student_id;
+        this.marks_cc = marks_cc;
+        this.marks_ml = marks_ml;
+        this.avg_marks = (marks_cc + marks_ml) / 2;
+    }
+
+    public void displayAvg() {
+        System.out.println("The average marks scored by " + name + " is " + avg_marks + ".");
+    }
+}
 
