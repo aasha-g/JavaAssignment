8787f78ad0747d9fe996f06579807ec8a1b7e49f
aasha-g <aashagowda1095@gmail.com>
2026-02-18 18:27:40 +0800

    Update NullPointerException.java

--- ExceptionHandling/NullPointerException.java
+++ ExceptionHandling/NullPointerException.java
@@ -1,1 +1,25 @@
+package exceptionHandling;
 
+public class NullPointer_Exception {
+
+	
+	public static void main(String[] args) {
+		
+	    System.out.println("Program is started.....");
+         
+	    String s = null;
+	    
+	    try
+	    {
+	    	System.out.println(s.length());
+	    }
+	    catch(NullPointerException e)
+	    {
+	    	System.out.println("Handled Exception...");
+	    	System.out.println(e.getMessage());
+	    }
+	    
+	    System.out.println("Program is completed.....");
+	}
+
+}
