ea6745af8b58d06efa9a28c943d5589dc7092e2f
aasha-g <aashagowda1095@gmail.com>
2026-02-18 18:26:22 +0800

    Update Finally.java

--- ExceptionHandling/Finally.java
+++ ExceptionHandling/Finally.java
@@ -1,1 +1,35 @@
+package exceptionHandling;
+
+import java.util.Scanner;
+
+public class FinallyDemo {
+
+	public static void main(String[] args) {
+		
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter a number: ");
+       int n = sc.nextInt();
+        
+        try {
+            
+            System.out.println(100 / n);
+        } 
+        catch (Exception e) 
+        {
+            System.out.println("Something went wrong!");
+        } 
+        finally 
+        {
+            
+            System.out.println("Closing the scanner...");
+            sc.close();
+        }
+        
+         System.out.println("Program is completed.....");
+    }
+
+
+	}
+
+
 
