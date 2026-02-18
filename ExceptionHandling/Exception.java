927b728b61ed3148c1c0cb845f292fc06459e23f
aasha-g <aashagowda1095@gmail.com>
2026-02-18 18:27:04 +0800

    Update Exception.java

--- ExceptionHandling/Exception.java
+++ ExceptionHandling/Exception.java
@@ -1,1 +1,30 @@
 
+package exceptionHandling;
+
+import java.util.Scanner;
+
+public class UnknowException {
+
+	public static void main(String[] args) {
+		System.out.println("Program is started.....");
+		 
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		
+        int n = sc.nextInt();
+        
+        try {
+         System.out.println(100/n);
+        }
+        catch(Exception e){
+        	
+        	 System.out.println("Invalid data"+e.getMessage());
+        }
+        System.out.println("Program is completed.....");
+        
+        
+        sc.close();
+
+	}
+
+}
