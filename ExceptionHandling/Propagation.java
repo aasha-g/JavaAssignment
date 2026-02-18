366ccc3c890a26ff2ae308f2652d34ca9ed96702
aasha-g <aashagowda1095@gmail.com>
2026-02-18 18:48:31 +0800

    Update Propagation.java

--- ExceptionHandling/Propagation.java
+++ ExceptionHandling/Propagation.java
@@ -1,1 +1,27 @@
+package exceptionHandling;
+
+public class Propagation {
+
+	public static void main(String[] args) {
+		
+		try 
+		{
+            manager();
+        } catch (NullPointerException e) 
+		{
+            System.out.println("Manager: I'll handle this error!");
+        }
+    }
+
+    static void manager() {
+        worker(); // calls the worker
+    }
+
+    static void worker() {
+        String text = null;
+        System.out.println(text.length()); // This triggers the error
+
+	}
+
+}
 
