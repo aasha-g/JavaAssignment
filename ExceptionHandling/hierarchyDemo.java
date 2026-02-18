7556aea0d0581f79cfb821b0ea49ce27c75c2c5e
aasha-g <aashagowda1095@gmail.com>
2026-02-18 18:30:32 +0800

    Update hierarchyDemo.java

--- ExceptionHandling/hierarchyDemo.java
+++ ExceptionHandling/hierarchyDemo.java
@@ -1,1 +1,21 @@
+package exceptionHandling;
+
+public class HierarchyDemo {
+    public static void main(String[] args) {
+       
+        Exception myEx = new NullPointerException("Oops!");
+
+        
+        System.out.println("Is it an Exception? " + (myEx instanceof Exception));
+        System.out.println("Is it a Throwable? " + (myEx instanceof Throwable));
+        
+        try 
+        {
+            int x = 10 / 0;
+        } catch (Throwable t) 
+        {
+            System.out.println("Caught by the grandparent: " + t.getClass().getSimpleName());
+        }
+    }
+}
 
