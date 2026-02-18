b0249bb255bfba23cd5c5d47d85bf36078abce48
aasha-g <aashagowda1095@gmail.com>
2026-02-18 18:54:32 +0800

    Update RethrowExample.java

--- ExceptionHandling/RethrowExample.java
+++ ExceptionHandling/RethrowExample.java
@@ -1,1 +1,31 @@
+package exceptionHandling;
+
+public class RethrowExample {
+
+    public static void withdraw(int amount) {
+        try {
+            if (amount > 1000) 
+            {
+               throw new ArithmeticException("Insufficient funds for large withdrawal.");
+            }
+            
+            System.out.println("Withdrawal successful: $" + amount);
+            
+            } catch (ArithmeticException e) 
+            {
+            System.out.println("Local Log: A withdrawal error occurred.");
+            
+            throw e; 
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            withdraw(5000); 
+        } catch (ArithmeticException e) {
+           
+            System.err.println("Main System Alert: " + e.getMessage());
+        }
+    }
+}
 
