e99ea342aad8eb2d8cfb14e91c58122235d27caa
aasha-g <aashagowda1095@gmail.com>
2026-02-18 18:47:23 +0800

    Update PrintStackTrace.java

--- ExceptionHandling/PrintStackTrace.java
+++ ExceptionHandling/PrintStackTrace.java
@@ -1,1 +1,24 @@
+package exceptionHandling;
 
+public class PrintStack {
+
+	public static void main(String[] args) {
+		try 
+		{
+            int result = 10 / 0; 
+            
+        } catch (ArithmeticException e) 
+		{
+            
+            System.out.println("Simple Message: " + e.getMessage());
+
+            System.out.println("-----------------------------------");
+
+            // 2. printStackTrace() - Detailed map
+            System.out.println("Full Trace:");
+            e.printStackTrace();
+        }
+    }
+
+
+	}
