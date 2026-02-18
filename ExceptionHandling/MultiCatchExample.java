d5e9b6f0b4f6c83baa27995a11a1081da7b718c6
aasha-g <aashagowda1095@gmail.com>
2026-02-18 18:39:18 +0800

    Update MultiCatchExample.java

--- ExceptionHandling/MultiCatchExample.java
+++ ExceptionHandling/MultiCatchExample.java
@@ -1,1 +1,28 @@
+package exceptionHandling;
+
+import java.io.IOException;
+
+public class MultiCatchExample {
+    public static void main(String[] args) {
+        try {
+            int denominator = 0; 
+            
+            //  ArithmeticException (Division by zero)
+            int result = 100 / denominator; 
+            
+            if (result > 0) 
+            {
+                throw new IOException("File not found!");
+            }
+
+            } catch (ArithmeticException | IOException e) 
+            {
+            // This one block handles BOTH types of errors
+            System.out.println("Error caught: " + e.getClass().getSimpleName());
+            System.out.println("Message: " + e.getMessage());
+            }
+        
+        System.out.println("Program continues...");
+    }
+}
 
