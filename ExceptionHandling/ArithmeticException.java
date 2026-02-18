c5d65abb080a329636079bbb55ca55a2df64d311
aasha-g <aashagowda1095@gmail.com>
2026-02-18 18:24:53 +0800

    Update ArithmeticException.java

--- ExceptionHandling/ArithmeticException.java
+++ ExceptionHandling/ArithmeticException.java
@@ -1,1 +1,35 @@
+package exceptionHandling;
+
+import java.util.Scanner;
+
+public class ArithmeticException_1 {
+
+	public static void main(String[] args) {
+		
+	    System.out.println("Program is started.....");
+		 
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		
+        int n = sc.nextInt();
+        
+       // System.out.println(100/n);
+        
+       try 
+        {
+         System.out.println(100/n);
+        }
+        catch(ArithmeticException e)
+        {
+        	
+        	 System.out.println("Invalid data");
+        }
+        
+        System.out.println("Program is completed.....");
+        
+        
+        sc.close();
+	}
+
+}
 
