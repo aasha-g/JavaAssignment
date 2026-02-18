f43a9adc6976b5d4d9eb0339fb30399010225cb8
aasha-g <aashagowda1095@gmail.com>
2026-02-18 18:25:45 +0800

    Update ArrayIndexOutOfBoundsException.java

--- ExceptionHandling/ArrayIndexOutOfBoundsException.java
+++ ExceptionHandling/ArrayIndexOutOfBoundsException.java
@@ -1,1 +1,35 @@
+package exceptionHandling;
+
+public class ArrayOutOfBound_exc {
+
+	public static void main(String[] args) {
+		
+        System.out.println("Program is started.....");
+        
+        int[] numbers = {10, 20, 30}; // This array has indexes 0, 1, and 2
+        
+        try {
+            // Trying to access index 5, which does not exist
+            System.out.println("Value at index 5: " + numbers[1]/1);
+            String s=null;
+            System.out.println(s.length());
+        } 
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("index that is out of range");
+        }
+        catch(ArithmeticException e)
+        {
+        	System.out.println("Airthmetic exception");
+        }
+        catch(Exception e)
+        {
+        	System.out.println(" exception occured");
+        }
+        
+        	
+        
+        System.out.println("Program is completed.....");
+    }
+
+	}
 
