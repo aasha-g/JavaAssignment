7ee436340dc8937b9401d8755343fbc89047b85c
aasha-g <aashagowda1095@gmail.com>
2026-02-18 18:56:00 +0800

    Update ThrowStatement.java

--- ExceptionHandling/ThrowStatement.java
+++ ExceptionHandling/ThrowStatement.java
@@ -1,1 +1,24 @@
+package exceptionHandling;
+
+public class ThrowStatement {
+	
+	 static void checkNumber(int num) {
+	        if (num < 0) 
+	        {
+	            throw new ArithmeticException("Negative number not allowed");
+	        }
+	            System.out.println("Valid number: " + num);
+	    }
+
+	public static void main(String[] args) {
+		
+		 try {
+	            checkNumber(-5);   // change value to test
+	         } catch (ArithmeticException e)
+		     {
+	            System.out.println("Exception caught: " + e.getMessage());
+	         }
+	}
+
+}
 
