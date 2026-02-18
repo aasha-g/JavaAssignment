4d4a69fd9a6faccfd631f0eef4d4e106e61041cd
aasha-g <aashagowda1095@gmail.com>
2026-02-18 18:29:15 +0800

    Update BankSystem.java

--- ExceptionHandling/BankSystem.java
+++ ExceptionHandling/BankSystem.java
@@ -1,1 +1,35 @@
+package exceptionHandling;
 
+//1. The Custom Exception (Not public)
+class LowBalanceException extends Exception {
+ public LowBalanceException(String message) {
+     super(message);
+ }
+}
+
+//2. The Main Class (Matches the filename)
+public class BankSystem {
+ 
+ public static void withdraw(int balance, int amount) throws LowBalanceException {
+     System.out.println("Current Balance: $" + balance);
+     System.out.println("Trying to withdraw: $" + amount);
+
+     if (amount > balance) {
+         // Throw our custom error if the math doesn't work
+         throw new LowBalanceException("Transaction Denied: Insufficient Funds!");
+     }
+
+     System.out.println("Success! New Balance: $" + (balance - amount));
+ }
+
+ public static void main(String[] args) {
+     try {
+         // This will trigger the exception
+         withdraw(500, 1000); 
+         
+     } catch (LowBalanceException e) {
+         // Catching our specific custom error
+         System.err.println("Error: " + e.getMessage());
+     }
+ }
+}
