48085bce7b8603ab5f435fcfb26b52b7a9c07be3
aasha-g <aashagowda1095@gmail.com>
2026-02-18 18:35:10 +0800

    Update Logging.java

--- ExceptionHandling/Logging.java
+++ ExceptionHandling/Logging.java
@@ -1,1 +1,12 @@
+package exceptionHandling;
 
+public class Logging {
+    public static void main(String[] args) {
+        try {
+            int A = 10 / 0; // Trigger an error
+        } catch (ArithmeticException e) {
+            // Log the error message to the error stream
+            System.err.println("LOG ERROR: Math went wrong. " + e.getMessage());
+        }
+    }
+}
