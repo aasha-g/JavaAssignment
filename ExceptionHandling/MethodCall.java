e83df54b32ab8c060f5250de8297a614d0128eb3
aasha-g <aashagowda1095@gmail.com>
2026-02-18 18:36:09 +0800

    Update MethodCall.java

--- ExceptionHandling/MethodCall.java
+++ ExceptionHandling/MethodCall.java
@@ -1,1 +1,25 @@
+package exceptionHandling;
 
+public class MethodCall {
+
+	public static void main(String[] args) {
+		
+		try 
+		{
+            divide(10, 0);
+        } catch (ArithmeticException e) 
+		{
+            System.out.println("Error handled in main: You cannot divide by zero!");
+        }
+    }
+
+    
+    static void divide(int num, int divisor) {
+        if (divisor == 0) {
+            throw new ArithmeticException();
+        }
+        System.out.println((num / divisor));
+
+	}
+
+}
