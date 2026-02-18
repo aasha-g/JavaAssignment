d35d0e754847de46b91e49d683ca42e57bc0e663
aasha-g <aashagowda1095@gmail.com>
2026-02-18 18:56:59 +0800

    Update VotingCheck.java

--- ExceptionHandling/VotingCheck.java
+++ ExceptionHandling/VotingCheck.java
@@ -1,1 +1,33 @@
+package exceptionHandling;
+
+class AgeException extends Exception {//user defined exception
+ public AgeException(String message) {
+     super(message);
+ }
+}
+
+public class VotingCheck {
+
+ static void checkAge(int age) throws AgeException {
+     if (age < 18) {
+         throw new AgeException("You are not eligible to vote.");
+     } else {
+         System.out.println("You are eligible to vote.");
+     }
+ }
+
+ public static void main(String[] args) {
+     try {
+         checkAge(16);   // change age to test
+     } catch (AgeException e) {
+         System.out.println("Exception caught: " + e.getMessage());
+     }
+     try {
+		checkAge(20);
+	} catch (AgeException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+ }
+}
 
