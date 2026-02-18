b21e65ce13f21d35dce98f21792d3d0c54dae322
aasha-g <aashagowda1095@gmail.com>
2026-02-18 18:43:52 +0800

    Update NumberFormat.java

--- ExceptionHandling/NumberFormat.java
+++ ExceptionHandling/NumberFormat.java
@@ -1,1 +1,18 @@
+package exceptionHandling;
+
+public class NumberFormatEx {
+
+	public static void main(String[] args) {
+		
+		String s = "15$"; 
+		
+        try {
+            int num = Integer.parseInt(s);
+            System.out.println("Result: " + num);
+            } catch (NumberFormatException e)
+            {
+            System.out.println("Invalid input: Not a number.");
+            }
+ }
+}
 
