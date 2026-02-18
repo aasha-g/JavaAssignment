f01a3dea52dcc05ba3b326102019e887e0b07452
aasha-g <aashagowda1095@gmail.com>
2026-02-18 18:41:18 +0800

    Update NegativeCheck.java

--- ExceptionHandling/NegativeCheck.java
+++ ExceptionHandling/NegativeCheck.java
@@ -1,1 +1,29 @@
+package exceptionHandling;
+
+public class NegativeCheck {
+	
+	static void checkNumber(int num) 
+	{
+        if (num < 0) 
+        {
+            throw new RuntimeException("Number cannot be negative");
+        }
+            System.out.println("Valid number: " + num);
+    }
+
+	public static void main(String[] args) {
+		
+		 try 
+		 {
+	         checkNumber(-5);   // change value to test
+	     } 
+		 catch (RuntimeException e) 
+		 {
+	         System.out.println("Exception caught: " + e.getMessage());
+	     }
+		
+		
+	}
+
+}
 
