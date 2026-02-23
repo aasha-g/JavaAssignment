66b1548be789e58868edb0bddf6b3d9faf694ee1
aasha-g <aashagowda1095@gmail.com>
2026-02-23 14:26:43 +0800

    Update MultiCatchExample.java

--- ExceptionHandling/MultiCatchExample.java
+++ ExceptionHandling/MultiCatchExample.java
@@ -1,3 +1,7 @@
+/*Assignment 2: Multi-Catch Block
+Catch IOException and ArithmeticException in a single catch block.
+*/
+
 package exceptionHandling;
 
 import java.io.IOException;
@@ -5,24 +9,25 @@ import java.io.IOException;
 public class MultiCatchExample {
     public static void main(String[] args) {
         try {
-            int denominator = 0; 
+            int A = 0; 
             
             //  ArithmeticException (Division by zero)
-            int result = 100 / denominator; 
+            int result = 100 / A; 
             
-            if (result > 0) 
+            if (result < 0) 
             {
                 throw new IOException("File not found!");
             }
 
             } catch (ArithmeticException | IOException e) 
             {
-            // This one block handles BOTH types of errors
-            System.out.println("Error caught: " + e.getClass().getSimpleName());
+            
+            System.out.println("Error caught: " + e.getClass());//full class name of the object 
             System.out.println("Message: " + e.getMessage());
             }
         
         System.out.println("Program continues...");
     }
-}
-
+}          /*Output: Error caught: class java.lang.ArithmeticException
+                     Message: / by zero
+                     Program continues...*/
