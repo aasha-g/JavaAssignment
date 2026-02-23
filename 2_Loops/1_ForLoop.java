7bda5837c8203f0325d68ed8a21cdebd09a3a12a
aasha-g <aashagowda1095@gmail.com>
2026-02-23 13:42:57 +0800

    Update 1_ForLoop.java

--- 2_Loops/1_ForLoop.java
+++ 2_Loops/1_ForLoop.java
@@ -1,3 +1,5 @@
+//5) Find the factorial of given number and print for factorial of 1.
+
 package internshipAssignments_1;
 
 import java.util.Scanner;
@@ -11,17 +13,19 @@ public class Factorial {
 		System.out.print("Enter a number : ");
 		int n = sc.nextInt();
 		
-		long factorial = 1;
+		long factorial = 1;                //n!=n(n-1)(n-2)....
 		
 		
-		for(int i=1;i<=n;i++)
+		for(int i=1;i<=n;i++)      //n=3
 		{
-			factorial *= i;
+			factorial *= i;        //1*1,1*2,(2*3=6)
 		}
 		
 		System.out.println("Factorial of "+n+ " is: "+factorial);
 		sc.close();
-
+   
+		//output:Enter a number : 3
+		//Factorial of 3 is: 6
 	}
         
 }
