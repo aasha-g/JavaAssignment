a61c3b43ee3782b772083969ae267e5505f97ffe
aasha-g <aashagowda1095@gmail.com>
2026-02-23 13:59:41 +0800

    Update and rename 3_ForLoopMultiplication.java to Multiplication.java

--- 2_Loops/Multiplication.java
+++ 2_Loops/Multiplication.java
@@ -1,3 +1,9 @@
+/*Assignment 2: Multiplication Table using For Loop
+Write a program that:
+Accepts an integer.
+Uses a for loop to print the multiplication table of that number (up to 10)*/
+
+
 package internshipAssignments_1;
 
 import java.util.Scanner;
@@ -25,7 +31,21 @@ public class Multiplication {
         	 i++;
         } */	 
         
+      /*  Output:Enter a number: 3
+                3x1=3
+        		3x2=6
+        		3x3=9
+        		3x4=12
+        		3x5=15
+        		3x6=18
+        		3x7=21
+        		3x8=24
+        		3x9=27
+        		3x10=30
+           */
+        
+        
 	}
+	
 
 }
-
