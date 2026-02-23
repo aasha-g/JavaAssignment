e96fb49a6b1dfedd02217eee87f01f06ecf9b33c
aasha-g <aashagowda1095@gmail.com>
2026-02-23 13:50:35 +0800

    Update and rename 1_WhileLoop.java to EvenAndOddNumbers.java

--- 2_Loops/EvenAndOddNumbers.java
+++ 2_Loops/EvenAndOddNumbers.java
@@ -1,3 +1,8 @@
+//) Given
+//int a = 641538927;
+//count the number of even and odd digits in the above number.
+
+
 package internshipAssignments_1;
 
 public class EvenAndOddNumbers {
@@ -9,18 +14,24 @@ public class EvenAndOddNumbers {
         int oCount = 0;
 
         while (a != 0) {
-            int digit = a % 10;
+            int digit = a % 10;   //Modulo operator gives remainder
             if (digit % 2 == 0) {
             	eCount++;
             } else {
             	oCount++; 
             }
-            a /= 10;
+            a /= 10;     //division:removes decimal part or last digit
         }
 
         System.out.println("Even digits: " + eCount);
         System.out.println("Odd digits: " + oCount);
 
+        
+       // output:Even digits: 4
+       // Odd digits: 5
 	}
+	
+	
+	
 
 }
