9797853788681e97c64cb6d2d5fc680dce00374e
aasha-g <aashagowda1095@gmail.com>
2026-02-23 14:21:29 +0800

    Update hierarchyDemo.java

--- ExceptionHandling/hierarchyDemo.java
+++ ExceptionHandling/hierarchyDemo.java
@@ -1,13 +1,16 @@
+/*Assignment 2: Exception Hierarchy
+Show how Throwable is the parent of Exception and Error.*/
+
 package exceptionHandling;
 
 public class HierarchyDemo {
     public static void main(String[] args) {
        
-        Exception myEx = new NullPointerException("Oops!");
+        Exception Ex = new NullPointerException();
 
         
-        System.out.println("Is it an Exception? " + (myEx instanceof Exception));
-        System.out.println("Is it a Throwable? " + (myEx instanceof Throwable));
+        System.out.println("Is it an Exception? " + (Ex instanceof Exception));
+        System.out.println("Is it a Throwable? " + (Ex instanceof Throwable));
         
         try 
         {
@@ -18,4 +21,6 @@ public class HierarchyDemo {
         }
     }
 }
-
+        /*Output: Is it an Exception? true
+          Is it a Throwable? true
+          Caught by the grandparent: ArithmeticException*/
