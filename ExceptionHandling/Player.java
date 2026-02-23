b9d21887511106ed71516782b3ef909d1e7569a1
aasha-g <aashagowda1095@gmail.com>
2026-02-23 14:39:29 +0800

    Update Player.java

--- ExceptionHandling/Player.java
+++ ExceptionHandling/Player.java
@@ -1,7 +1,49 @@
 package exceptionHandling;
 
 public class Player {
+	
+/*Assignment : Raising Exception in Constructor
+Throw an exception in a constructor if invalid values are passed.*/
+
+package exceptionHandling;
+
+public class Player {
 	private String name;
+    private int score;
+
+    public Player(String name, int score) {
+         if (score < 0) 
+         {
+        	 throw new IllegalArgumentException("Score cannot be negative!: " + score);
+         }
+
+        this.name = name;
+        this.score = score;
+    }
+
+    public void display() {
+        System.out.println("Player: " + name + " , Score: " + score);
+    }
+
+	public static void main(String[] args) {
+		try {
+            // This will work fine
+            Player p1 = new Player("A", 50);
+            p1.display();
+
+            // This will trigger the exception
+            Player p2 = new Player("B", -10);
+            p2.display(); 
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error creating player: " + e.getMessage());
+		
+
+	}
+               //Output:Player: A , Score: 50
+		             //Error creating player: Score cannot be negative!: -10
+}
+} String name;
     private int score;
 
     public Player(String name, int score) {
