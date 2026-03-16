bd7eec7829dd5476cd39ab352c2f87a3cb49dc07
aasha-g <aashagowda1095@gmail.com>
2026-03-16 21:55:05 +0800

    Update Assessment4.java

--- Assessment/Assessment4.java
+++ Assessment/Assessment4.java
@@ -1,1 +1,24 @@
+package seleniumDemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assessment4 {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        driver.manage().window().maximize();
+        
+        driver.findElement(By.id("mousehover")).click();
+        boolean a=driver.findElement(By.linkText("Top")).isDisplayed();
+        System.out.println(a);
+        boolean b=driver.findElement(By.linkText("Reload")).isDisplayed();
+        System.out.println(b);
+        
+        
+	}
+
+}
 
