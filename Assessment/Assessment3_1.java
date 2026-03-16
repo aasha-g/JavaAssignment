9f3749aa617c34da69016de6a6e629ed53e39955
aasha-g <aashagowda1095@gmail.com>
2026-03-16 21:54:25 +0800

    Update Assessment3_1.java

--- Assessment/Assessment3_1.java
+++ Assessment/Assessment3_1.java
@@ -1,1 +1,29 @@
+package seleniumDemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assessment3_1 {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demo.automationtesting.in/Frames.html");
+        driver.manage().window().maximize();
+        
+        driver.findElement(By.linkText("Iframe with in an Iframe")).click();
+        
+        WebElement outerFrame=driver.findElement(By.xpath("//h5[text()='Nested iFrames']"));
+        driver.switchTo().frame(outerFrame);
+        
+        WebElement innerFrame=driver.findElement(By.xpath("//h5[text()='iFrame Demo']"));
+        
+        driver.switchTo().frame(innerFrame);
+        
+        driver.findElement(By.xpath("//input[type='text']")).sendKeys("Automation practice");
+        driver.switchTo().defaultContent();
+	}
+
+}
 
