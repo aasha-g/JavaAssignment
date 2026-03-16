263e59a8f9628b7c6ad1187a8d0065d2c1fd06d2
aasha-g <aashagowda1095@gmail.com>
2026-03-16 21:52:14 +0800

    Update Assessment2_1.java

--- Assessment/Assessment2_1.java
+++ Assessment/Assessment2_1.java
@@ -1,1 +1,22 @@
+package seleniumDemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assessment2_1 {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demo.automationtesting.in/Alerts.html");
+        driver.manage().window().maximize();
+        
+       
+        driver.findElement(By.xpath("//button[contains(text(),'click the button to display an  alert box:')]" )).click();
+        
+        driver.switchTo().alert().accept();
+        
+	}
+
+}
 
