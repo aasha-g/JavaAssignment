a28544e8c069ad66adf352ae3c0210175efbb8e3
aasha-g <aashagowda1095@gmail.com>
2026-03-16 21:51:28 +0800

    Update Assessment01.java

--- Assessment/Assessment01.java
+++ Assessment/Assessment01.java
@@ -1,1 +1,26 @@
+package seleniumDemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assessment01 {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://leafground.com/checkbox.xhtml");
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		driver.findElement(By.id("j_idt87:basic:0")).click();
+		
+		//driver.findElement(By.xpath("//input[@id='j_idt87:basic:0']")).click();
+		//
+		//driver.findElement(By.xpath("//input[@value='js']")).click();
+		//driver.findElement(By.xpath("//input[@value='c#']")).click();
+		//driver.findElement(By.xpath("//input[@value='ot']")).click();
+		
+		
+	}
+
+}
 
