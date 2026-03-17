a667e19322598b9c7e85bd2da034a86f7f5adfdf
aasha-g <aashagowda1095@gmail.com>
2026-03-17 11:41:11 +0800

    Update Assessment1_4.java

--- Assessment/Assessment_1/Assessment1_4.java
+++ Assessment/Assessment_1/Assessment1_4.java
@@ -1,1 +1,21 @@
+package seleniumDemo;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assessment1_4 {
+	
+
+	public static void main(String[] args) {
+
+
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://leafground.com/dashboard.xhtml");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.xpath("//td[@role='gridcell']")).click();
+
+
+}
+}
