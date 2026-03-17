44514a2f550b4ebe7814710ee250a004b0079813
aasha-g <aashagowda1095@gmail.com>
2026-03-17 11:40:25 +0800

    Update Assessment1.java

--- Assessment/Assessment_1/Assessment1.java
+++ Assessment/Assessment_1/Assessment1.java
@@ -1,1 +1,37 @@
+package seleniumDemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assessment1 {
+
+	public static void main(String[] args) {
+		
+		
+			WebDriver driver=new ChromeDriver();
+			driver.get("https://leafground.com/dashboard.xhtml");
+			driver.manage().window().maximize();
+			
+			driver.findElement(By.xpath("//input[@id='email']")).click();
+
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
 
