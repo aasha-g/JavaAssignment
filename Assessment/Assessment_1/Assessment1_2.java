15d5d29a5160986eaea0a40194d1ed600a438db1
aasha-g <aashagowda1095@gmail.com>
2026-03-17 11:38:49 +0800

    Update Assessment1_2.java

--- Assessment/Assessment_1/Assessment1_2.java
+++ Assessment/Assessment_1/Assessment1_2.java
@@ -1,1 +1,19 @@
+package seleniumDemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assessment1_2 {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://leafground.com/radio.xhtml");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.xpath("//input[@value='Option1']")).click();
+
+	}
+
+}
 
