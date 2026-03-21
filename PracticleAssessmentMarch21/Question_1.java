71f6217528862c6a4d106d41cc0f90399c50fc2a
aasha-g <aashagowda1095@gmail.com>
2026-03-21 14:28:25 +0800

    Update Question_1.java

--- PracticleAssessmentMarch21/Question_1.java
+++ PracticleAssessmentMarch21/Question_1.java
@@ -1,1 +1,18 @@
+package assessmentCollection;
+
+import java.util.ArrayList;
+
+import assessmentCollection.Question1.Passenger;
+
+public class Question_1 {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Passenger>mylist=new ArrayList<>();
+		
+		mylist.add(new Passenger(1,"A",20,12));
+		
+	}
+
+}
 
