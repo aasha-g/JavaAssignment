5102fcf82a42d788a97286862ee513ffbbf03779
aasha-g <aashagowda1095@gmail.com>
2026-03-21 14:27:37 +0800

    Update Question1.java

--- PracticleAssessmentMarch21/Question1.java
+++ PracticleAssessmentMarch21/Question1.java
@@ -1,1 +1,37 @@
 
+package assessmentCollection;
+
+public class Question1 {
+	
+	class Passenger{
+		int passengerId;
+		String name;
+		int age;
+		int seatNumber;
+	
+	Passenger(int passengerId,String name,int age,int seatNumber){
+		this.passengerId=passengerId;
+		this.name=name;
+		this.age=age;
+		this.seatNumber=seatNumber;
+		
+	}
+	}
+	
+	class Bus{
+		int busID;
+		int totalSeats;
+		int availableSeats;
+		
+		Bus(int busID,int totalSeats,int availableSeats){
+			this.busID=busID;
+			this.totalSeats=totalSeats;
+			this.availableSeats=availableSeats;
+		}
+		
+	}
+	public static void main(String[] args) {
+		
+	}
+
+}
